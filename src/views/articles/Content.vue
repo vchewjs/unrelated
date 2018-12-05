7651d16dae12c7a2f9b6c159a12dda69d7effe50
95html <3193607330@qq.com>
2018-12-05 16:06:37 +0800

    文章的再编辑以及删除操作

--- src/views/articles/Content.vue
+++ src/views/articles/Content.vue
@@ -12,6 +12,12 @@
           <div class="entry-content">
             <div class="content-body entry-content panel-body">
               <div class="markdown-body" v-html="content"></div>
+              <div v-if="auth && uid === 1" class="panel-footer operate">
+  <div class="actions">
+    <a @click="deleteArticle" class="admin" href="javascript:;"><i class="fa fa-trash-o"></i></a>
+    <a @click="editArticle" class="admin" href="javascript:;"><i class="fa fa-pencil-square-o"></i></a>
+  </div>
+</div>
             </div>
           </div>
         </div>
@@ -22,6 +28,7 @@
 
 <script>
 import SimpleMDE from "simplemde";
+import { mapState } from 'vuex'
 
 export default {
   name: "Content",
@@ -29,16 +36,25 @@ export default {
     return {
       title: "", // 文章标题
       content: "", // 文章内容
-      date: "" // 创建时间
+      date: "", // 创建时间
+      uid: 1 // 用户 ID
     };
+  },
+  // 添加计算属性
+  computed: {
+    // 将仓库的以下状态混入到计算属性之中
+    ...mapState([
+      'auth',
+      'user'
+    ])
   },
   created() {
     const articleId = this.$route.params.articleId;
     const article = this.$store.getters.getArticleById(articleId);
 
     if (article) {
-      let { title, content, date } = article;
-
+      let { uid, title, content, date } = article;
+      this.uid = uid;
       this.title = title;
       this.content = SimpleMDE.prototype.markdown(content);
       this.date = date;
@@ -47,6 +63,25 @@ export default {
         this.$el.querySelectorAll("pre code").forEach(el => {});
       });
     }
+    this.articleId = articleId
+  },
+  methods: {
+    // 编辑文章
+    editArticle() {
+      // 点击编辑文章图标，跳到编辑文章页面，并附带当前文章 ID
+      this.$router.push({ name: 'Edit', params: { articleId: this.articleId } })
+    },
+    // 删除文章
+    deleteArticle() {
+  this.$swal({
+    text: '你确定要删除此内容吗?',
+    confirmButtonText: '删除'
+  }).then((res) => {
+    if (res.value) {
+      this.$store.dispatch('post', { articleId: this.articleId })
+    }
+  })
+}
   }
 };
 </script>
